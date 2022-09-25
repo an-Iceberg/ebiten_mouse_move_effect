82e4ede4c431c8abe2ef2e62d257c8825883c188
an-Iceberg <a.g.marosi@bluewin.ch>
2022-09-25 19:03:30 +0200

    init

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"ebiten_balls/balls"
+	"ebiten_balls/vec2d"
 	"image/color"
 	"log"
 	"math/rand"
@@ -9,45 +9,46 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Game struct{}
 
 // Global variables
 var (
-	black        color.Color = color.RGBA{R: 0, G: 0, B: 0, A: 255}
-	Balls        []balls.Ball
-	ScreenWidth  = 800
-	ScreenHeight = 600
+	Black         color.Color = color.RGBA{R: 0, G: 0, B: 0, A: 255}
+	Lime          color.Color = color.RGBA{R: 0, G: 255, B: 0, A: 255}
+	ScreenWidth               = 800
+	ScreenHeight              = 600
+	Mouse         vec2d.Vec2D
+	PreviousMouse vec2d.Vec2D
 )
 
+func MouseHasMoved() bool {
+	x, y := ebiten.CursorPosition()
+	Mouse.X = float64(x)
+	Mouse.Y = float64(y)
+
+	returnValue := !Mouse.IsEqualTo(PreviousMouse)
+
+	PreviousMouse = Mouse
+
+	return returnValue
+}
+
 func (game *Game) Update() error {
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
-		Balls = balls.AddBalls(Balls, ScreenWidth, ScreenHeight)
-	}
-
-	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonRight) && len(Balls) > 0 {
-		Balls = balls.RemoveBalls(Balls)
-	}
-
-	Balls = balls.UpdateBalls(Balls, ScreenWidth, ScreenHeight)
-
 	return nil
 }
 
 func (game *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(black)
+	screen.Fill(Black)
 
-	// mouseX, mouseY := ebiten.CursorPosition()
-
-	for _, ball := range Balls {
-		ebitenutil.DrawCircle(screen, float64(ball.Position.X), float64(ball.Position.Y), float64(ball.Radius), ball.Color)
+	if MouseHasMoved() {
+		ebitenutil.DrawCircle(screen, Mouse.X, Mouse.Y, 30, Lime)
 	}
 }
 
 func (game *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return outsideWidth, outsideHeight
+	return ScreenWidth, ScreenHeight
 }
 
 func main() {
@@ -57,7 +58,7 @@ func main() {
 
 	ebiten.SetWindowPosition(600, 200)
 	ebiten.SetWindowSize(ScreenWidth, ScreenHeight)
-	ebiten.SetWindowTitle("Balls!")
+	ebiten.SetWindowTitle("Mouse Move Effects")
 
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
